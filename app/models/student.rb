7ca298c71fec21b648736748f377187cab2200a6
Letícia Vidal <leticiavna@id.uff.br>
2018-03-06 15:39:40 -0300

    linking local database and heroku

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,2 +1,15 @@
 class Student < ApplicationRecord
+  belongs_to :university, optional: true
+  belongs_to :ej, optional: true
+
+
+  has_secure_password
+
+  validates :name, presence: true, length: { in: 2..30 }
+  validates :surname, presence: true, length: {in: 2.. 40 }
+  validates :email, presence: true, length: { in: 4 .. 40 }, uniqueness: true,
+   format: { with: /\A[a-z]{2,30}\w\@id.uff.br\z/,
+    message: "Insira um email nome@id.uff.br"}
+  validates :password, length: { in: 6..16 }, allow_nil: true
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180306154501) do
+ActiveRecord::Schema.define(version: 20180306183017) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20180306154501) do
     t.datetime "updated_at", null: false
     t.bigint "career_id"
     t.bigint "university_id"
+    t.string "password_digest"
     t.index ["career_id"], name: "index_students_on_career_id"
     t.index ["university_id"], name: "index_students_on_university_id"
   end

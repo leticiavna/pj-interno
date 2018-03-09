a1202c02a3a04d6564ae8c7cf2ad4ff68285f29c
Letícia Vidal <leticiavna@id.uff.br>
2018-03-08 21:24:44 -0300

    colocando a jwt para autenticação

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   resources :careers
   resources :universities
   resources :students
+
+    post 'authenticate', to: 'authentication#authenticate'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

d7538e21931d16b6eb16948a1e20a6c037dfd58f
Letícia Vidal <leticiavna@id.uff.br>
2018-03-13 15:10:19 -0300

    schema alterado

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180306183017) do
+ActiveRecord::Schema.define(version: 20180313163151) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,7 +32,7 @@ ActiveRecord::Schema.define(version: 20180306183017) do
   create_table "students", force: :cascade do |t|
     t.string "name"
     t.string "surname"
-    t.string "email"
+    t.string "email", default: "", null: false
     t.datetime "birthday"
     t.text "description"
     t.boolean "gender"
@@ -42,7 +42,28 @@ ActiveRecord::Schema.define(version: 20180306183017) do
     t.bigint "career_id"
     t.bigint "university_id"
     t.string "password_digest"
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet "current_sign_in_ip"
+    t.inet "last_sign_in_ip"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.string "provider", default: "email", null: false
+    t.string "uid", default: "", null: false
+    t.boolean "allow_password_change", default: false
+    t.json "tokens"
     t.index ["career_id"], name: "index_students_on_career_id"
+    t.index ["confirmation_token"], name: "index_students_on_confirmation_token", unique: true
+    t.index ["email"], name: "index_students_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_students_on_reset_password_token", unique: true
+    t.index ["uid", "provider"], name: "index_students_on_uid_and_provider", unique: true
     t.index ["university_id"], name: "index_students_on_university_id"
   end
 

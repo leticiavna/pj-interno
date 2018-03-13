d57b76a90dad0db0061b983eed03e415066823e2
Letícia Vidal <leticiavna@id.uff.br>
2018-03-13 10:52:26 -0300

    cnosertando rota

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
   resources :universities
   resources :students
 
-  get 'authenticate', to: 'authentication'
   post 'authenticate', to: 'authentication#authenticate'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
